e1cbff5e16b9889c479226a204a772612c2facf0
Stephanie Lorraine Ignas <53800465+ignasstephanie@users.noreply.github.com>
2025-04-27 15:00:13 -0700

    Small fixes to styling in about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,9 +18,8 @@ function About()
             <div className="content-container">
                 <div className="text-left paragraph-container">
                     <h1 className="heading-intro medium-text" data-aos="fade-up" data-aos-anchor-placement="center-bottom">
-
-                        Hello there! I am <br/>
-                        <span className="gradient-text">Stephanie Lorraine Ignas</span>
+                        Hello there! I am
+                        <span className="gradient-text name-gradient"> Stephanie Lorraine Ignas</span>
                     </h1>
                     <p className='description-container' data-aos="fade-right" data-aos-anchor="#example-anchor" data-aos-offset="500" data-aos-duration="500" data-aos-delay="200">
                         With a keen eye for design and a mastery of code, I work seamlessly as a 
@@ -79,52 +78,52 @@ function About()
                         </div>
                     </div>
                 </div>
-                <h2 className="heading" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="200">My Tech <span className="gradient-text">Stack</span></h2>
+                <h2 className="heading margin-top-gap" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="200">My Tech <span className="gradient-text">Stack</span></h2>
                 <div className="padding-large">
-                    <div className="paragraph-container h-flex">
-                        <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="400">
-                            <h4>Frontend</h4>
-                            <div className="wrap-container">
-                                {techstack
-                                    .filter(t => t.type === 'frontend')
-                                    .map((techstack, index) => (
-                                        <Label key={index} {...techstack} />
-                                ))}
-                            </div>
-                        </div>
-                        <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="400">
-                            <h4>Backend</h4>
-                            <div className="wrap-container">
-                                {techstack
-                                    .filter(t => t.type === 'backend')
-                                    .map((techstack, index) => (
-                                        <Label key={index} {...techstack} />
-                                ))}
-                            </div>
+                <div className="paragraph-container h-flex">
+                    <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="400">
+                        <h4>Frontend</h4>
+                        <div className="wrap-container">
+                            {techstack
+                                .filter(t => t.type === 'frontend')
+                                .map((techstack, index) => (
+                                    <Label key={index} {...techstack} />
+                            ))}
                         </div>
                     </div>
-                    <div className="paragraph-container h-flex">
-                        <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="700">
-                            <h4>UI Design</h4>
-                            <div className="wrap-container">
-                                {techstack
-                                    .filter(t => t.type === 'uidesign')
-                                    .map((techstack, index) => (
-                                        <Label key={index} {...techstack} />
-                                ))}
-                            </div>
-                        </div>
-                        <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="700">
-                            <h4>Graphic Design</h4>
-                            <div className="wrap-container">
-                                {techstack
-                                    .filter(t => t.type === 'graphicdesign')
-                                    .map((techstack, index) => (
-                                        <Label key={index} {...techstack} />
-                                ))}
-                            </div>
+                    <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="400">
+                        <h4>Backend</h4>
+                        <div className="wrap-container">
+                            {techstack
+                                .filter(t => t.type === 'backend')
+                                .map((techstack, index) => (
+                                    <Label key={index} {...techstack} />
+                            ))}
                         </div>
                     </div>
+                </div>
+                <div className="paragraph-container h-flex">
+                    <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="700">
+                        <h4>UI Design</h4>
+                        <div className="wrap-container">
+                            {techstack
+                                .filter(t => t.type === 'uidesign')
+                                .map((techstack, index) => (
+                                    <Label key={index} {...techstack} />
+                            ))}
+                        </div>
+                    </div>
+                    <div className="v-flex width-half" data-aos="fade-up" data-aos-anchor-placement="top-bottom" data-aos-delay="700">
+                        <h4>Graphic Design</h4>
+                        <div className="wrap-container">
+                            {techstack
+                                .filter(t => t.type === 'graphicdesign')
+                                .map((techstack, index) => (
+                                    <Label key={index} {...techstack} />
+                            ))}
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
             <Footer />
